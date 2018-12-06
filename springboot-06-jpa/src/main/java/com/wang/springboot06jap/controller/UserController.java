07886e240f04841cffde8536096753d8953effbd
WangXiaYun <814752288@qq.com>
2018-12-06 18:22:58 +0800

    jpa

--- springboot-06-jpa/src/main/java/com/wang/springboot06jap/controller/UserController.java
+++ springboot-06-jpa/src/main/java/com/wang/springboot06jap/controller/UserController.java
@@ -2,11 +2,13 @@ package com.wang.springboot06jap.controller;
 
 import com.wang.springboot06jap.entity.User;
 import com.wang.springboot06jap.repository.UserRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class UserController {
@@ -16,7 +18,10 @@ public class UserController {
     @GetMapping("/user/{id}")
     @ResponseBody
     public User getUser(@PathVariable("id") Integer id){
-        User one = userRepository.getOne(id);
-        return one;
+        User user = userRepository.findById(id).get();
+       // System.out.println(userRepository.getOne(id));
+
+        //
+        return user;
     }
 }
